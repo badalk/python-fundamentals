99846408e3aa8a7b021ed13c397545932f5add9b
Badal Kotecha <badalkotecha@Badals-MBP.home>
2021-09-26 14:06:07 -0400

    chore: adding examples for exceptions

---
+++ 06-Exceptions/REPL.py
@@ -0,0 +1,6 @@
+# ###1 Basic exception handling
+
+from exceptional import convert
+convert("33") # works
+convert("hedgehog") # throws ValueError
+
---
+++ 06-Exceptions/exceptional.py
@@ -0,0 +1,17 @@
+# Exceptions are means of control flow
+# difficult to demonstrate on REPL
+
+'''A module for demonstrating exception'''
+def convert(s):
+    '''Convert to an integer'''
+    x = int(s)
+    return x
+
+def convert(s):
+    try:
+        x = int(s)
+    except:
+
+
+
+
